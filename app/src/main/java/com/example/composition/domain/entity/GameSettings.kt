ece22a52ee3b586812f3b9d2063b0209ba625f9d
Lira <lir0na@mail.ru>
2023-07-17 17:44:07 +0300

    Created presentation layer: 4 Fragments ant navigation between them

--- app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -1,8 +1,12 @@
 package com.example.composition.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class GameSettings(
     val maxSumValue: Int, //максимальное значение суммы
     val minCountOfRightAnswers: Int,//мин. значение правильных ответов для победы
     val minPercentOfRightAnswers: Int,//мин. значение правильных ответов в % для победы
     val gameTimeInSecond: Int //время игры
-)
+): Parcelable
